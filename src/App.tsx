196b84996ac4d3deea0de30cc85e12edf1f587c4
Piotr <hunter976@gmail.com>
2025-12-07 09:03:19 +0100

    Added notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
 export default function App() {
   return <h1 className="text-red-400">Hello World</h1>;
 }
+
+//HEADER
+//Product Page
+//Image component (control it with css breakpoints)
+//Type
+//Title
+//Info
+//Price (with props discout %)
+//add to cart component
